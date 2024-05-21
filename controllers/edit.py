665f42fef25c227f43b724e7d071de739e1086c2
krzysztofmarciniak <krzysztofmarciniak@skiff.com>
2024-05-21 14:47:25 +0200

    remove unused files

--- controllers/edit.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request, session, redirect, url_for
-from model import MySQLModel
-class EditManager:
-    def __init__(self):
-        pass
-
-    def edit_form(self):
-        return render_template('edit.html', title='Edit Page', content='')
